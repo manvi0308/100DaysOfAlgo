c486cf87cf88e66040859073938cdc9db900eaea
manvi0308 <60390722+manvi0308@users.noreply.github.com>
2020-09-06 12:04:19 +0530

    Create LinkedListIntroduction.py

---
+++ Day 7/LinkedListIntroduction.py
@@ -0,0 +1,26 @@
+# Linked list traversal
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+    
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print(temp.data)
+            temp = temp.next
+
+if __name__ == '__main__':
+    llist = LinkedList() # To initialize a linked list object having head as Null
+    llist.head = Node(1) #Inserting 1 as the first node
+    second = Node(2)
+    third = Node(3)
+
+    llist.head.next = second
+    second.next     = third
+    llist.printList()
+
