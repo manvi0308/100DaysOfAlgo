680adb37706808d5dc7f20ed2bc5a1fdef62d82c
manvi0308 <60390722+manvi0308@users.noreply.github.com>
2020-11-19 20:07:20 +0530

    Create SortingUsingTrivialHashFunction.py

---
+++ Day 73/SortingUsingTrivialHashFunction.py
@@ -0,0 +1,33 @@
+# Python3 program to sort an array 
+# using hash function 
+def sortUsingHash(a, n): 
+
+	# find the maximum element 
+	Max = max(a) 
+
+	# create a hash function upto 
+	# the max size 
+	Hash = [0] * (Max + 1) 
+
+	# traverse through all the elements 
+	# and keep a count 
+	for i in range(0, n): 
+		Hash[a[i]] += 1
+
+	# Traverse upto all elements and check 
+	# if it is present or not. If it is 
+	# present, then print the element the 
+	# number of times it's present. Once we 
+	# have printed n times, that means we 
+	# have printed n elements so break out 
+	# of the loop 
+	for i in range(0, Max + 1): 
+
+		# if present 
+		if Hash[i] != 0: 
+
+			# print the element that number 
+			# of times it's present 
+			for j in range(0, Hash[i]): 
+				print(i, end = " ") 
+
