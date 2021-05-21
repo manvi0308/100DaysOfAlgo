9c6cdb5e047f8b88e7429878b1e1a1ee93bae981
Manvi Chaddha <60390722+manvi0308@users.noreply.github.com>
2021-05-21 18:20:40 +0530

    Update LinkedListInsertion.java

--- Day 15/LinkedListInsertion.java
+++ Day 15/LinkedListInsertion.java
@@ -1,10 +1,15 @@
 
 // This will contain the code fr Linked List insertion in java
+
+
+// Node Implementation
 class Node
 {
   int data;
   Node next;
   
+  
+  // constructor for node
   Node(int d)
   {
     this.data = d;
